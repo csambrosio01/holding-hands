e149d52354c5ae014e60db1c1556145eb3bca5e6
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-08 18:12:07 -0300

    Add missing class App, that is used in User class

---
+++ app/src/main/java/com/usp/holdinghands/App.kt
@@ -0,0 +1,17 @@
+package com.usp.holdinghands
+
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+    }
+
+    companion object {
+        var context: Context? = null
+            private set
+    }
+}
