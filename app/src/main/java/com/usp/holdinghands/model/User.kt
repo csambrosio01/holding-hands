9ab8e7b9a19fea5f94a09acf23da15349ea826a3
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-15 18:09:27 -0300

    Fix imports in UserController (this will probably break build)

--- app/src/main/java/com/usp/holdinghands/model/User.kt
+++ app/src/main/java/com/usp/holdinghands/model/User.kt
@@ -2,15 +2,27 @@ package com.usp.holdinghands.model
 
 import com.usp.holdinghands.App
 import com.usp.holdinghands.R
-import java.lang.StringBuilder
 
-data class User(val name: String, val age: Int, val distance: String, val helpTypes: List<String>, val image: String)
+enum class Gender {
+    MALE, FEMALE, BOTH
+}
+
+enum class HelpType(val type: String) {
+    TYPE_1(App.context!!.getString(R.string.filter_category_1_switch)),
+    TYPE_2(App.context!!.getString(R.string.filter_category_2_switch)),
+    TYPE_3(App.context!!.getString(R.string.filter_category_3_switch)),
+    TYPE_4(App.context!!.getString(R.string.filter_category_4_switch)),
+    TYPE_5(App.context!!.getString(R.string.filter_category_5_switch)),
+    ALL(App.context!!.getString(R.string.filter_category_6_switch))
+}
+
+data class User(val name: String, val age: Int, val distance: Double, val helpTypes: List<HelpType>, val image: String, val gender: Gender)
 
 fun User.getHelpAsString(): String {
     val sb = StringBuilder()
     sb.append(App.context?.resources?.getText(R.string.user_help_types_header))
     for (value in helpTypes) {
-        sb.append(" - $value\n")
+        sb.append(" - ${value.type}\n")
     }
     return sb.toString().trim()
 }
