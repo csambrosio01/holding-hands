91091d8da0efb1548cd55b3fda6226cca167409f
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-15 18:49:00 -0300

    Add method in UserController to make search based on filters

--- app/src/main/java/com/usp/holdinghands/controller/UserController.kt
+++ app/src/main/java/com/usp/holdinghands/controller/UserController.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.usp.holdinghands.R
+import com.usp.holdinghands.model.Gender
+import com.usp.holdinghands.model.HelpType
 import com.usp.holdinghands.model.User
+import com.usp.holdinghands.model.UserFilter
 import java.io.InputStreamReader
 import java.lang.reflect.Type
 
@@ -26,5 +29,35 @@ class UserController(val context: Context) {
 
     fun fromJsonString(jsonString: String): List<User> {
         return gson.fromJson(jsonString, listUserType)
+    }
+
+    fun makeSearch(userFilter: UserFilter): List<User> {
+        val users = getUsers()
+        val a = users.filter { user ->
+            shouldIncludeUser(userFilter, user)
+        }
+        return a
+    }
+
+    private fun shouldIncludeUser(userFilter: UserFilter, user: User): Boolean {
+        var isValid = true
+
+        if (userFilter.gender != Gender.BOTH) {
+            if (user.gender != userFilter.gender) isValid = false
+        }
+
+        if (user.age < userFilter.ageMin || user.age > userFilter.ageMax) isValid = false
+
+        if (user.distance > userFilter.distance) isValid = false
+
+        var contains = false
+        if (!userFilter.helpTypes.contains(HelpType.ALL)) {
+            for (helpType in user.helpTypes) {
+                if (userFilter.helpTypes.contains(helpType)) contains = true
+            }
+            if (!contains) isValid = false
+        }
+
+        return isValid
     }
 }
