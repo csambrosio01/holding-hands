eb375cd65e69832a7fbcf5958b99e580b6f8c74e
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-20 17:50:58 -0300

    Add NavigationActivity with minor logic to switch tabs (does not implement home fragment yet)

---
+++ app/src/main/java/com/usp/holdinghands/NavigationActivity.kt
@@ -0,0 +1,41 @@
+package com.usp.holdinghands
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.usp.holdinghands.fragments.HelpFragment
+import com.usp.holdinghands.fragments.HomeFragment
+import com.usp.holdinghands.fragments.NotificationFragment
+import com.usp.holdinghands.fragments.UserProfileFragment
+
+class NavigationActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment.newInstance()
+    private val helpFragment = HelpFragment.newInstance()
+    private val notificationFragment = NotificationFragment.newInstance()
+    private val userProfileFragment = UserProfileFragment.newInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+
+        replaceFragment(homeFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_home -> { }
+                R.id.bottom_help_request -> replaceFragment(helpFragment)
+                R.id.bottom_notification -> replaceFragment(notificationFragment)
+                R.id.bottom_profile -> replaceFragment(userProfileFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/usp/holdinghands/fragments/HelpFragment.kt
@@ -0,0 +1,23 @@
+package com.usp.holdinghands.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.usp.holdinghands.R
+
+class HelpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_help, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = HelpFragment()
+    }
+}
