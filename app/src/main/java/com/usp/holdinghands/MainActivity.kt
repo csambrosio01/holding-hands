d3880fb624f2b0db73c440e3141252e5e6051995
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-21 15:31:27 -0300

    Move NavigationActivity to activities package

--- app/src/main/java/com/usp/holdinghands/MainActivity.kt
+++ app/src/main/java/com/usp/holdinghands/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Handler
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import com.usp.holdinghands.activities.NavigationActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
