e662e10e12d6ba99a083a97d3221b26ea9b726ab
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-21 17:38:52 -0300

    Correctly close all activities when users logs in

--- app/src/main/java/com/usp/holdinghands/activities/LoginActivity.kt
+++ app/src/main/java/com/usp/holdinghands/activities/LoginActivity.kt
@@ -19,8 +19,10 @@ class LoginActivity : AppCompatActivity() {
             //TODO: Make login
 
             val intent = Intent(this, NavigationActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                    Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
-            finish()
         }
     }
 }
