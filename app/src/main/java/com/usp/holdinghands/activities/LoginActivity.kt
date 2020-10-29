9ab25c7ad4701cf1c613461a233c37152d497555
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-10-29 19:26:44 -0300

    Add validators to LoginActivity

--- app/src/main/java/com/usp/holdinghands/activities/LoginActivity.kt
+++ app/src/main/java/com/usp/holdinghands/activities/LoginActivity.kt
@@ -4,30 +4,68 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.textfield.TextInputLayout
 import com.usp.holdinghands.R
+import com.usp.holdinghands.utils.validators.EmailValidator
+import com.usp.holdinghands.utils.validators.PasswordValidator
+import com.usp.holdinghands.utils.validators.Validator
 
 class LoginActivity : AppCompatActivity() {
+
+    private val validators = mutableListOf<Validator>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         setupButtons()
+        setupValidators()
     }
 
     private fun setupButtons() {
         findViewById<Button>(R.id.bt_login).setOnClickListener {
-            //TODO: Make login
+            if (validateFields()) {
+                //TODO: Make login
 
-            val intent = Intent(this, NavigationActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
-                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
-                    Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
+                val intent = Intent(this, NavigationActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            } else {
+                //TODO: Show error message?
+            }
         }
 
         findViewById<Button>(R.id.bt_sign_up).setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setupValidators() {
+        val emailTextInputLayout = findViewById<TextInputLayout>(R.id.login_email)
+        val emailValidator = EmailValidator(true, emailTextInputLayout)
+        emailTextInputLayout.editText!!.onFocusChangeListener = emailValidator
+
+        val passwordTextInputLayout = findViewById<TextInputLayout>(R.id.login_password)
+        val passwordValidator = PasswordValidator(true, passwordTextInputLayout)
+        passwordTextInputLayout.editText!!.onFocusChangeListener = passwordValidator
+
+        validators.addAll(
+            mutableListOf(
+                emailValidator,
+                passwordValidator
+            )
+        )
+    }
+
+    private fun validateFields(): Boolean {
+        var isValid = true
+        validators.forEach {
+            it.validate()
+            if (!it.isValid) isValid = false
+        }
+        return isValid
     }
 }
