1ef0cb3cae2cdf9963efab9cb5f815a0f7cf675b
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-04 17:56:35 -0300

    Create a companion object to initialize App Database

---
+++ app/src/main/java/com/usp/holdinghands/persistence/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.usp.holdinghands.persistence
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.usp.holdinghands.model.User
+import com.usp.holdinghands.model.UserConverters
+import com.usp.holdinghands.persistence.dao.UserDao
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+@TypeConverters(UserConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
