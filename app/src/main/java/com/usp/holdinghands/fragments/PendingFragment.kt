575f1c1c45e938289efdc39f6cf03eef06568b52
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-25 21:12:27 -0300

    Add PendingFragment, that will be used in help tab

---
+++ app/src/main/java/com/usp/holdinghands/fragments/PendingFragment.kt
@@ -0,0 +1,98 @@
+package com.usp.holdinghands.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.usp.holdinghands.R
+import com.usp.holdinghands.adapter.OnItemClickListener
+import com.usp.holdinghands.adapter.UserAdapter
+import com.usp.holdinghands.controller.UserController
+import com.usp.holdinghands.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class PendingFragment : Fragment(), OnItemClickListener {
+
+    private val users = mutableListOf<User>()
+    private lateinit var userController: UserController
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        userController = UserController(activity!!.applicationContext)
+
+        configureRecyclerView()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    private fun configureRecyclerView() {
+        viewManager = LinearLayoutManager(activity!!.applicationContext)
+        viewAdapter = UserAdapter(users, activity!!.applicationContext, true, this)
+
+        recyclerView = view!!.findViewById<RecyclerView>(R.id.help_recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        CoroutineScope(Dispatchers.Main + Job()).launch {
+            users.addAll(userController.getHelpRequests())
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun notifyDataSetChanged() {
+        val emptyView = view!!.findViewById<TextView>(R.id.empty_view)
+        if (users.isEmpty()) {
+            recyclerView.visibility = View.GONE
+            emptyView.visibility = View.VISIBLE
+        } else {
+            emptyView.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            viewAdapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onAccept(position: Int) {
+        users.removeAt(position)
+        notifyDataSetChanged()
+        Toast.makeText(
+            activity!!.applicationContext,
+            activity!!.applicationContext.getString(R.string.help_accepted),
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onDeny(position: Int) {
+        users.removeAt(position)
+        notifyDataSetChanged()
+        Toast.makeText(
+            activity!!.applicationContext,
+            activity!!.applicationContext.getString(R.string.help_denied),
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PendingFragment()
+    }
+}
