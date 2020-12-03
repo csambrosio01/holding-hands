99b812dfc18d15fa18579d40fe2ac3a3a39f7f04
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-02 21:51:18 -0300

    Add logic to read from inputs and create object to send to API, also add logic to call api to create an user

--- app/src/main/java/com/usp/holdinghands/activities/SignupActivity.kt
+++ app/src/main/java/com/usp/holdinghands/activities/SignupActivity.kt
@@ -6,6 +6,8 @@ import android.location.Location
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -17,8 +19,15 @@ import com.google.android.material.textfield.TextInputLayout
 import com.usp.holdinghands.LocationService
 import com.usp.holdinghands.R
 import com.usp.holdinghands.controller.UserController
+import com.usp.holdinghands.model.Gender
+import com.usp.holdinghands.model.HelpType
+import com.usp.holdinghands.model.LoginResponse
+import com.usp.holdinghands.model.UserDTO
 import com.usp.holdinghands.utils.MaskEditUtil
 import com.usp.holdinghands.utils.validators.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 const val PERMISSION_GRANTED_REQUEST_CODE = 156
 
@@ -140,18 +149,91 @@ class SignupActivity : AppCompatActivity(), ValidatorActivity, LocationService {
     }
 
     override fun onLocationResult(location: Location) {
-        createUser()
+        createUser(location)
     }
 
     override fun mainButtonClicked() {
         getLocation()
     }
 
-    private fun createUser() {
-        val intent = Intent(applicationContext, NavigationActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
-                Intent.FLAG_ACTIVITY_CLEAR_TASK or
-                Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
+    private fun makeUser(location: Location): UserDTO {
+        val checkedRadioButtonId =
+            findViewById<RadioGroup>(R.id.sign_up_gender).checkedRadioButtonId
+
+        val helpTypes = mutableListOf<HelpType>()
+
+        if (findViewById<SwitchCompat>(R.id.help_type__1_switch).isChecked) helpTypes.add(
+            HelpType.TYPE_1
+        )
+        if (findViewById<SwitchCompat>(R.id.help_type__2_switch).isChecked) helpTypes.add(
+            HelpType.TYPE_2
+        )
+        if (findViewById<SwitchCompat>(R.id.help_type__3_switch).isChecked) helpTypes.add(
+            HelpType.TYPE_3
+        )
+        if (findViewById<SwitchCompat>(R.id.help_type__4_switch).isChecked) helpTypes.add(
+            HelpType.TYPE_4
+        )
+        if (findViewById<SwitchCompat>(R.id.help_type__5_switch).isChecked) helpTypes.add(
+            HelpType.TYPE_5
+        )
+
+        if (helpTypes.isEmpty() && findViewById<SwitchCompat>(R.id.sign_up_is_volunteer_switch).isChecked) {
+            //TODO: show error message
+        }
+
+        return UserDTO(
+            name = findViewById<TextInputLayout>(R.id.sign_up_name).editText!!.text.toString(),
+            helpTypes = helpTypes,
+            gender = if (findViewById<RadioButton>(checkedRadioButtonId) == findViewById(R.id.sign_up_male)) Gender.MALE else Gender.FEMALE,
+            profession = findViewById<TextInputLayout>(R.id.sign_up_profession).editText!!.text.toString(),
+            birth = getBirthDate(findViewById<TextInputLayout>(R.id.sign_up_birth).editText!!.text.toString()),
+            email = findViewById<TextInputLayout>(R.id.sign_up_email).editText!!.text.toString(),
+            phone = MaskEditUtil.unmask("55" + findViewById<TextInputLayout>(R.id.sign_up_phone).editText!!.text.toString()),
+            password = findViewById<TextInputLayout>(R.id.sign_up_password).editText!!.text.toString(),
+            isHelper = findViewById<SwitchCompat>(R.id.sign_up_is_volunteer_switch).isChecked,
+            latitude = location.latitude,
+            longitude = location.longitude
+        )
+    }
+
+    private fun getBirthDate(input: String): String {
+        val inputList = input.split("/")
+        val stringBuilder = StringBuilder(inputList[2])
+
+        stringBuilder.append("-")
+        stringBuilder.append(inputList[1])
+        stringBuilder.append("-")
+        stringBuilder.append(inputList[0])
+
+        return stringBuilder.toString()
+    }
+
+    private fun createUser(location: Location) {
+        val user = makeUser(location)
+
+        findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.VISIBLE
+        userController.createUser(user, object : Callback<LoginResponse> {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.GONE
+                if (response.isSuccessful && response.body() != null) {
+                    val loginResponse = response.body()!!
+                    userController.setLogin(loginResponse)
+
+                    val intent = Intent(applicationContext, NavigationActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                            Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                } else {
+                    //TODO: Show error message
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                print(t.localizedMessage)
+                // TODO: Show error message
+            }
+        })
     }
 }
