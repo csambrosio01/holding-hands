a41f3bd715b5b01949cc342b63ac616832cccc9d
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-03 22:22:45 -0300

    Fix UserActivity to work with new UserResponse object

--- app/src/main/java/com/usp/holdinghands/activities/UserActivity.kt
+++ app/src/main/java/com/usp/holdinghands/activities/UserActivity.kt
@@ -11,13 +11,15 @@ import com.usp.holdinghands.adapter.IS_PENDING_VIEW
 import com.usp.holdinghands.adapter.USER
 import com.usp.holdinghands.model.Gender
 import com.usp.holdinghands.model.User
+import com.usp.holdinghands.model.UserResponse
 import com.usp.holdinghands.model.getHelpAsString
+import com.usp.holdinghands.utils.EnumConverter
 import com.usp.holdinghands.utils.JsonUtil
 import com.usp.holdinghands.utils.MaskEditUtil
 
 class UserActivity : AppCompatActivity() {
 
-    private lateinit var user: User
+    private lateinit var user: UserResponse
     private var isPendingView: Boolean = false
     private var isHistoryView: Boolean = false
 
@@ -35,7 +37,7 @@ class UserActivity : AppCompatActivity() {
 
     private fun configureViews() {
         val imageId = applicationContext.resources.getIdentifier(
-            user.image,
+            user.imageId ?: (if (user.gender == Gender.MALE) "lucas" else "heloise"),
             "drawable",
             applicationContext.packageName
         )
@@ -43,24 +45,34 @@ class UserActivity : AppCompatActivity() {
         findViewById<ImageView>(R.id.user_image).setImageResource(imageId)
         findViewById<TextView>(R.id.user_name).text = user.name
         findViewById<TextView>(R.id.user_rating).text = applicationContext.getString(R.string.user_rating, user.rating.toString())
-        findViewById<TextView>(R.id.user_age).text = applicationContext.getString(R.string.user_age, user.age.toString())
+        findViewById<TextView>(R.id.user_age).text = applicationContext.getString(R.string.user_age, 15.toString())
         findViewById<TextView>(R.id.user_distance).text = getString(
             R.string.user_distance_long,
-            user.distance.toString()
+            5.0.toString()
         )
-        findViewById<TextView>(R.id.user_help_types).text = user.getHelpAsString()
+
+        if (user.isHelper) {
+            findViewById<TextView>(R.id.user_help_types).text = EnumConverter.getHelpAsString(
+                EnumConverter.stringToEnumList(user.helpTypes)
+            )
+
+            //        if (user.numberOfHelps <= 1) {
+//            findViewById<TextView>(R.id.user_number_helps).text = applicationContext.getString(R.string.user_number_helps_singular, user.numberOfHelps.toString())
+//        } else {
+            findViewById<TextView>(R.id.user_number_helps).text = applicationContext.getString(R.string.user_number_helps_plural, 20.toString())
+//        }
+
+        } else {
+            findViewById<TextView>(R.id.user_help_types).text = getString(R.string.filter_supported_radio)
+            findViewById<TextView>(R.id.user_number_helps).visibility = View.GONE
+        }
+
         findViewById<TextView>(R.id.user_profession).text = user.profession
 
         when (user.gender) {
             Gender.FEMALE -> findViewById<TextView>(R.id.user_gender).text = applicationContext.getString(R.string.filter_female_switch)
             Gender.MALE -> findViewById<TextView>(R.id.user_gender).text = applicationContext.getString(R.string.filter_male_switch)
-            Gender.BOTH -> { findViewById<TextView>(R.id.user_gender).visibility = View.GONE }
-        }
-
-        if (user.numberOfHelps <= 1) {
-            findViewById<TextView>(R.id.user_number_helps).text = applicationContext.getString(R.string.user_number_helps_singular, user.numberOfHelps.toString())
-        } else {
-            findViewById<TextView>(R.id.user_number_helps).text = applicationContext.getString(R.string.user_number_helps_plural, user.numberOfHelps.toString())
+            Gender.BOTH -> findViewById<TextView>(R.id.user_gender).visibility = View.GONE
         }
 
         findViewById<TextView>(R.id.user_email).text = user.email
