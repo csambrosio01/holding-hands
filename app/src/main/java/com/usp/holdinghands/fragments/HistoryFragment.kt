6fd35b3759ab7aa35ffc1fc75085bccf766355de
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-08 20:30:58 -0300

    Get Matchs from API, also refact code to use MatchAdapter

--- app/src/main/java/com/usp/holdinghands/fragments/HistoryFragment.kt
+++ app/src/main/java/com/usp/holdinghands/fragments/HistoryFragment.kt
@@ -5,23 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.usp.holdinghands.R
-import com.usp.holdinghands.adapter.UserAdapter
-import com.usp.holdinghands.controller.UserController
-import com.usp.holdinghands.model.User
-import com.usp.holdinghands.model.UserResponse
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import com.usp.holdinghands.adapter.MatchAdapter
+import com.usp.holdinghands.controller.MatchController
+import com.usp.holdinghands.model.MatchResponse
+import com.usp.holdinghands.model.MatchStatus
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HistoryFragment : Fragment() {
 
-    private val users = mutableListOf<UserResponse>()
-    private lateinit var userController: UserController
+    private val matchs = mutableListOf<MatchResponse>()
+    private lateinit var matchController: MatchController
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
@@ -30,7 +30,7 @@ class HistoryFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        userController = UserController(activity!!.applicationContext)
+        matchController = MatchController(activity!!.applicationContext)
 
         configureRecyclerView()
     }
@@ -44,18 +44,51 @@ class HistoryFragment : Fragment() {
 
     private fun configureRecyclerView() {
         viewManager = LinearLayoutManager(activity!!.applicationContext)
-        viewAdapter = UserAdapter(users, activity!!.applicationContext, isHistoryView = true)
+        viewAdapter = MatchAdapter(matchs, activity!!.applicationContext, isHistoryView = true)
 
         recyclerView = view!!.findViewById<RecyclerView>(R.id.help_recycler_view).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
+
+        getMatchs()
+    }
+
+    private fun getMatchs() {
+        view!!.findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.VISIBLE
+        view!!.findViewById<RecyclerView>(R.id.help_recycler_view).visibility = View.GONE
+
+        matchController.getMatchs(MatchStatus.HISTORY, object : Callback<List<MatchResponse>> {
+            override fun onResponse(
+                call: Call<List<MatchResponse>>,
+                response: Response<List<MatchResponse>>
+            ) {
+                view!!.findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.GONE
+                view!!.findViewById<RecyclerView>(R.id.help_recycler_view).visibility = View.VISIBLE
+                if (response.isSuccessful && response.body() != null) {
+                    val matchList = response.body()!!
+                    matchs.clear()
+                    matchs.addAll(matchList)
+                } else {
+                    //TODO: Show error message
+                }
+
+                notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<List<MatchResponse>>, t: Throwable) {
+                view!!.findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.GONE
+                view!!.findViewById<RecyclerView>(R.id.help_recycler_view).visibility = View.VISIBLE
+                notifyDataSetChanged()
+                // TODO: Show error message
+            }
+        })
     }
 
     private fun notifyDataSetChanged() {
         val emptyView = view!!.findViewById<TextView>(R.id.empty_view)
-        if (users.isEmpty()) {
+        if (matchs.isEmpty()) {
             recyclerView.visibility = View.GONE
             emptyView.visibility = View.VISIBLE
         } else {
