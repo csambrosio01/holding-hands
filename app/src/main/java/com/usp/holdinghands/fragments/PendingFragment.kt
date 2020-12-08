02732838cecbd69c9affd762cdd443de18df0d43
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-08 20:32:52 -0300

    Get Matchs from API, also refact code to use MatchAdapter in PendingActivity

--- app/src/main/java/com/usp/holdinghands/fragments/PendingFragment.kt
+++ app/src/main/java/com/usp/holdinghands/fragments/PendingFragment.kt
@@ -6,19 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.usp.holdinghands.R
+import com.usp.holdinghands.adapter.MatchAdapter
 import com.usp.holdinghands.adapter.OnItemClickListener
-import com.usp.holdinghands.adapter.UserAdapter
-import com.usp.holdinghands.controller.UserController
-import com.usp.holdinghands.model.UserResponse
+import com.usp.holdinghands.controller.MatchController
+import com.usp.holdinghands.model.MatchResponse
+import com.usp.holdinghands.model.MatchStatus
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PendingFragment : Fragment(), OnItemClickListener {
 
-    private val users = mutableListOf<UserResponse>()
-    private lateinit var userController: UserController
+    private val matchs = mutableListOf<MatchResponse>()
+    private lateinit var matchController: MatchController
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
@@ -27,7 +32,7 @@ class PendingFragment : Fragment(), OnItemClickListener {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        userController = UserController(activity!!.applicationContext)
+        matchController = MatchController(activity!!.applicationContext)
 
         configureRecyclerView()
     }
@@ -41,18 +46,53 @@ class PendingFragment : Fragment(), OnItemClickListener {
 
     private fun configureRecyclerView() {
         viewManager = LinearLayoutManager(activity!!.applicationContext)
-        viewAdapter = UserAdapter(users, activity!!.applicationContext, true, listener = this)
+        viewAdapter = MatchAdapter(matchs, activity!!.applicationContext, isPendingView = true, listener = this)
 
         recyclerView = view!!.findViewById<RecyclerView>(R.id.help_recycler_view).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
+
+        getMatchs()
+    }
+
+    private fun getMatchs() {
+        view!!.findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.VISIBLE
+        view!!.findViewById<RecyclerView>(R.id.help_recycler_view).visibility = View.GONE
+
+        matchController.getMatchs(MatchStatus.PENDING, object : Callback<List<MatchResponse>> {
+            override fun onResponse(
+                call: Call<List<MatchResponse>>,
+                response: Response<List<MatchResponse>>
+            ) {
+                view!!.findViewById<ConstraintLayout>(R.id.progress_layout).visibility =
+                    View.GONE
+                view!!.findViewById<RecyclerView>(R.id.help_recycler_view).visibility =
+                    View.VISIBLE
+                if (response.isSuccessful && response.body() != null) {
+                    val matchList = response.body()!!
+                    matchs.clear()
+                    matchs.addAll(matchList)
+                } else {
+                    //TODO: Show error message
+                }
+
+                notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<List<MatchResponse>>, t: Throwable) {
+                view!!.findViewById<ConstraintLayout>(R.id.progress_layout).visibility = View.GONE
+                view!!.findViewById<RecyclerView>(R.id.help_recycler_view).visibility = View.VISIBLE
+                notifyDataSetChanged()
+                // TODO: Show error message
+            }
+        })
     }
 
     private fun notifyDataSetChanged() {
         val emptyView = view!!.findViewById<TextView>(R.id.empty_view)
-        if (users.isEmpty()) {
+        if (matchs.isEmpty()) {
             recyclerView.visibility = View.GONE
             emptyView.visibility = View.VISIBLE
         } else {
@@ -63,7 +103,7 @@ class PendingFragment : Fragment(), OnItemClickListener {
     }
 
     override fun onAccept(position: Int) {
-        users.removeAt(position)
+        matchs.removeAt(position)
         notifyDataSetChanged()
         Toast.makeText(
             activity!!.applicationContext,
@@ -73,7 +113,7 @@ class PendingFragment : Fragment(), OnItemClickListener {
     }
 
     override fun onDeny(position: Int) {
-        users.removeAt(position)
+        matchs.removeAt(position)
         notifyDataSetChanged()
         Toast.makeText(
             activity!!.applicationContext,
