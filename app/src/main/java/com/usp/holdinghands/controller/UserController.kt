c8f71b24b6ecb19d96911c3eacb8be2617f12321
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-09 21:20:03 -0300

    Remove unused method

--- app/src/main/java/com/usp/holdinghands/controller/UserController.kt
+++ app/src/main/java/com/usp/holdinghands/controller/UserController.kt
@@ -93,26 +93,4 @@ class UserController(val context: Context) {
         val call = request.rate(token, rating)
         call.enqueue(listener)
     }
-
-    private fun shouldIncludeUser(userFilter: UserFilter, user: User): Boolean {
-        var isValid = true
-
-        if (userFilter.gender != Gender.BOTH) {
-            if (user.gender != userFilter.gender) isValid = false
-        }
-
-        if (user.age < userFilter.ageMin || user.age > userFilter.ageMax) isValid = false
-
-        if (user.distance > userFilter.distance) isValid = false
-
-        var contains = false
-        if (!userFilter.helpTypes.contains(HelpType.ALL)) {
-            for (helpType in user.helpTypes) {
-                if (userFilter.helpTypes.contains(helpType)) contains = true
-            }
-            if (!contains) isValid = false
-        }
-
-        return isValid
-    }
 }
